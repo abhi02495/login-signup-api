565b78a54f1798bb1b118281925afe39feeb8f5d
abhi02495 <abhi.edu.2495@gmail.com>
2022-07-20 00:22:45 +0530

    Login endpoint fixed full

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -3,39 +3,28 @@ import { verifyUser, checkUser } from "../services/loginService.js";
 
 const router = express.Router();
 
-
 router.post("/login", async (req, res) => {
+  let email = req.body.email;
+  let application = req.body.application;
+  let password = req.body.password;
 
-    let email = req.body.email;
-    let application = req.body.application;
-    let password = req.body.password;
+  const user = await checkUser(email, application);
+  let result;
+  if (user) {
+    result = await verifyUser(password, user.password);
+    console.log(result);
+  } else {
+    result = {
+      message: "No user found",
+    };
+    res.status(400).send(result);
+    return;
+  }
 
-    // here the details will be passed to service where the info will be checked
-    // will receive response whether user is valid or not
-
-    const user = await checkUser(email, application);
-    console.log(user.password);
-    let result;
-    if(user){
-        result = await verifyUser(password, user.password);
-        console.log(result);
-    }
-    else {
-        result = {
-            message: "No user found"
-        };
-    }
-
-
-    // const salt = await bcrypt.genSalt(10);
-    // password = await bcrypt.hash(password, salt)
-
-    // const result = await bcrypt.compare(password, user.password);
-
-    res.send({
-        email: email,
-        authenticated: result,
-    })
-})
+  res.status(200).send({
+    email: email,
+    authenticated: result,
+  });
+});
 
 export default router;
--- services/loginService.js
+++ services/loginService.js
@@ -1,38 +1,24 @@
 import bcrypt from "bcrypt";
 import { checkUserInDB } from "../repositories/repository.js";
+import { json } from "express";
 
 export const checkUser = async (email, application) => {
-  // let user = {
-  //   email: "absh@gmail.com",
-  //   application: "invito",
-  //   authenticated: true,
-  //   password: "$2b$10$2orr0af4siSIEoptrZevvOkZA5ciKzw3h7ThfQHsKuiRyTPLySSm.",
-  // };
-
   const isUserinDb = await checkUserInDB(email, application);
   return isUserinDb;
 };
 
 export const verifyUser = async (password, hash) => {
-  // console.log(hash);
-  let password_comp_result;
-  await bcrypt.compare(password, hash, function(err, res){
-    if(err){
-      throw new Error('bcrypt failed!')
+  const data = await bcrypt.compare(password, hash);
+  if(!data){
+    return {
+      success: false,
+      message: 'Password is incorrect'
     }
-    if(res){
-      password_comp_result = {
-        success: true,
-        message: 'Login successful'
-      }
+  }
+  else {
+    return {
+      success: true,
+      message: 'Passwords match.'
     }
-    else {
-      password_comp_result = {
-        success: false,
-        message: 'Passwords not matching.'
-      }
-    }
-  });
-
-  return password_comp_result;
+  }
 };
