0f2ece41c1057fdbc82cd80e84ba2e23abab809a
abhi02495 <abhi.edu.2495@gmail.com>
2022-07-22 23:56:10 +0530

    cleanup

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import cors from 'cors';
-import mock from './mock.js';
 import bodyParser from 'body-parser';
 import loginRoutes from './controllers/loginController.js';
 import signUpRoutes from './controllers/signUpController.js';
@@ -8,15 +7,6 @@ const app = express();
  
 app.use(cors());
 app.use(bodyParser.json());
-
-app.get('/api/v1', (req, res) => {
-  res
-    .status(200)
-    .send(mock)
-    .end();
-});
- 
-
 
 app.use('/api/v1', loginRoutes);
 app.use('/api/v1', signUpRoutes);
--- mock.js
+++
@@ -1,7 +0,0 @@
-const mock = {
-    fullName: "Abhishek",
-    email: "ab.sh.2495@gmail.com",
-    password: "qwerewqdnwcdiwefroiwnefcwdjkcnwerfoiwenfrwefsndcjkscsd"
-}
-
-export default mock;
