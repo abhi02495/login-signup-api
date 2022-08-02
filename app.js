bacd2f04246f4bb58d5e1ce9e9b6aa860b0e86c3
abhi02495 <abhi.edu.2495@gmail.com>
2022-08-02 05:32:23 +0530

    verify jwt

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import loginRoutes from './controllers/loginController.js';
 import signUpRoutes from './controllers/signUpController.js';
+import validateUserRoutes from './controllers/validateUserController.js'
 const app = express();
  
 app.use(cors());
@@ -10,5 +11,6 @@ app.use(bodyParser.json());
 
 app.use('/api/v1', loginRoutes);
 app.use('/api/v1', signUpRoutes);
+app.use('/api/v1', validateUserRoutes);
 
 export default app;
---
+++ controllers/validateUserController.js
@@ -0,0 +1,28 @@
+import express from "express";
+import { APPLICATION_CONSTANTS } from "../common/constants.js";
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const router = express.Router();
+
+router.post('/validateUser', (req,res) => {
+    
+    let jwtSecretKey = process.env.JWT_SECRET_KEY
+
+    try{
+        let accessToken = req.body.accessToken;
+        const verified = jwt.verify(accessToken, jwtSecretKey);
+        if(verified){
+            return res.send("Successfully Verified");
+        }else{
+            // Access Denied
+            return res.status(401).send(error);
+        }
+    }catch(error){
+        return res.status(401).send(error);
+    }
+    
+})
+
+export default router;
