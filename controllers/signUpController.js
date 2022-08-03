12c13a7b666ab35266189b3e1456b81098f0cb46
abhi02495 <abhi.edu.2495@gmail.com>
2022-08-04 03:13:17 +0530

    sending jwt in successful signup

--- controllers/signUpController.js
+++ controllers/signUpController.js
@@ -2,6 +2,7 @@ import express from "express";
 import { userSignUp } from "../services/signUpService.js";
 import { Validations } from "../common/validations.js";
 import { APPLICATION_CONSTANTS } from "../common/constants.js";
+import { generateJwtToken } from "../services/loginService.js";
 
 const router = express.Router();
 
@@ -19,8 +20,11 @@ router.post("/signup", async (req, res) => {
   }
 
   let result;
+  let accessToken = "";
   if (password == confirmPassword) {
     result = await userSignUp(email, application, password);
+    if(result.isAdded)
+      accessToken = await generateJwtToken(result);
   } else {
     res
       .status(400)
@@ -28,7 +32,10 @@ router.post("/signup", async (req, res) => {
     return;
   }
 
-  res.send(result);
+  res.send({
+    result,
+    accessToken
+  });
 });
 
 export default router;
